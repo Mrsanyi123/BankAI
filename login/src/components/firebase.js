1fd4fa6ba132a87d240fc200270c919c96042485
Samuel-5485 <samediriba54@gmail.com>
2024-07-22 16:35:00 +0300

    login commited

---
+++ login/src/components/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaik65tgG1-XILSA8YskL88jqkBsB4Bb4",
+  authDomain: "login-with-multilinguai.firebaseapp.com",
+  projectId: "login-with-multilinguai",
+  storageBucket: "login-with-multilinguai.appspot.com",
+  messagingSenderId: "780169646730",
+  appId: "1:780169646730:web:d3dbc06763c326fda95373"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
